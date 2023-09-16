3e722590ece3032efda43e3a18de93d38d702a7c
A4SWSR <145178072+A4SWSR@users.noreply.github.com>
2023-09-16 01:34:47 -0300

    Update settings.py

--- Projeto_GPS/settings.py
+++ Projeto_GPS/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates', 'static')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'templates', 'static')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'templates', 'media')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'templates', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
